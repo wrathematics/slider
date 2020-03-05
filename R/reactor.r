f57bb55e13cb623c66790c762ffae7d53b3cd7cb
Drew Schmidt <wrathematics@gmail.com>
2020-03-05 13:02:07 -0500

    fix reactor

--- R/reactor.r
+++ R/reactor.r
@@ -1,22 +1,3 @@
-# Copyright (C) 2017 by Drew Schmidt <wrathematics@gmail.com>
-# 
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-# PERFORMANCE OF THIS SOFTWARE
-
-# Changes: 
-#    * stop() calls replaced with comm.stop()
-#    * add check.is.shaq()
-#    * add check.is.matrix()
-#    * add check.is.function()
-
 is.badval <- function(x)
 {
   is.na(x) || is.nan(x) || is.infinite(x)
@@ -64,7 +45,7 @@ check.is.string <- function(x)
   if (!is.string(x))  
 	{
     nm <- deparse(substitute(x))
-    comm.stop(paste0("argument '", nm, "' must be a single string"), call.=FALSE)
+    stop(paste0("argument '", nm, "' must be a single string"), call.=FALSE)
   }
 	
   invisible(TRUE)
@@ -83,7 +64,7 @@ check.is.int <- function(x)
   if (!is.numeric(x) || is.annoying(x) || !is.inty(x))  
   {
     nm <- deparse(substitute(x))
-    comm.stop(paste0("argument '", nm, "' must be an integer"), call.=FALSE)
+    stop(paste0("argument '", nm, "' must be an integer"), call.=FALSE)
   }
   
   invisible(TRUE)
@@ -94,7 +75,7 @@ check.is.natnum <- function(x)
   if (!is.numeric(x) || is.annoying(x) || !is.inty(x) || is.negative(x))  
   {
     nm <- deparse(substitute(x))
-    comm.stop(paste0("argument '", nm, "' must be a natural number (0 or positive integer)"), call.=FALSE)
+    stop(paste0("argument '", nm, "' must be a natural number (0 or positive integer)"), call.=FALSE)
   }
   
   invisible(TRUE)
@@ -105,7 +86,7 @@ check.is.posint <- function(x)
   if (!is.posint(x))
   {
     nm <- deparse(substitute(x))
-    comm.stop(paste0("argument '", nm, "' must be a positive integer"), call.=FALSE)
+    stop(paste0("argument '", nm, "' must be a positive integer"), call.=FALSE)
   }
   
   invisible(TRUE)
@@ -116,53 +97,7 @@ check.is.flag <- function(x)
   if (!is.flag(x))
   {
     nm <- deparse(substitute(x))
-    comm.stop(paste0("argument '", nm, "' must be TRUE or FALSE"), call.=FALSE)
-  }
-  
-  invisible(TRUE)
-}
-
-check.is.shaq <- function(x)
-{
-  if (!is.shaq(x))
-  {
-    nm <- deparse(substitute(x))
-    comm.stop(paste0("argument '", nm, "' must be a shaq"), call.=FALSE)
-  }
-  
-  invisible(TRUE)
-}
-
-check.is.matrix <- function(x)
-{
-  if (!is.matrix(x))
-  {
-    nm <- deparse(substitute(x))
-    comm.stop(paste0("argument '", nm, "' must be a matrix"), call.=FALSE)
-  }
-  
-  invisible(TRUE)
-}
-
-check.is.function <- function(x)
-{
-  if (!is.function(x))  
-	{
-    nm <- deparse(substitute(x))
-    comm.stop(paste0("argument '", nm, "' must be a function"), call.=FALSE)
-  }
-	
-  invisible(TRUE)
-}
-
-
-
-check <- function(x)
-{
-  if (!isTRUE(x))
-  {
-    nm <- deparse(substitute(x))
-    comm.stop(paste0("'", nm, "' is not TRUE"), call.=FALSE)
+    stop(paste0("argument '", nm, "' must be TRUE or FALSE"), call.=FALSE)
   }
   
   invisible(TRUE)
