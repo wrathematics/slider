5562a8cb3ad6feaac4f8dfff6737246ca3f26471
Drew Schmidt <wrathematics@gmail.com>
2020-03-05 12:55:40 -0500

    more main bindings

--- R/slide.r
+++ R/slide.r
@@ -1,12 +1,40 @@
+#' slide
+#' 
+#' TODO
+#' 
+#' @param trainfile,testfile
+#' TODO
+#' @param logfile
+#' TODO
+#' @param batchsize
+#' TODO
+#' @param rehash
+#' TODO
+#' @param rebuild
+#' TODO
+#' @param input_dim
+#' TODO
+#' @param Lr
+#' TODO
+#' @param epochs
+#' TODO
+#' @param stepsize
+#' TODO
+#' @param layers
+#' TODO
+#' 
+#' @importFrom filesampler wc_l
 #' @useDynLib slider R_slide
 #' @export
-slide = function(trainfile, testfile, batchsize=1000, rehash=1000, rebuild=1000, input_dim=784, Lr=0.0001, epochs=5, stepsize=20, layers=3)
+slide = function(trainfile, testfile, logfile="/tmp/log.txt", batchsize=1000,
+  rehash=1000, rebuild=1000, input_dim=784, Lr=0.0001, epochs=5, stepsize=20,
+  layers=3)
 {
   check.is.posint(batchsize)
   check.is.posint(rehash)
   check.is.posint(rebuild)
   check.is.posint(input_dim)
-  # Lr
+  # TODO Lr
   check.is.posint(epochs)
   check.is.posint(stepsize)
   check.is.posint(layers)
@@ -14,10 +42,17 @@ slide = function(trainfile, testfile, batchsize=1000, rehash=1000, rebuild=1000,
   trainfile = check_file(trainfile)
   testfile = check_file(testfile)
   
+  wc_train = filesampler::wc_l(trainfile) - 1
+  wc_test = filesampler::wc_l(testfile) - 1
+  
   addparam = function(name, val) paste0(name, "=", val, "\n")
   config = paste0(
     addparam("trainData", trainfile),
     addparam("testData", testfile),
+    addparam("totRecords", wc_train),
+    addparam("totRecordsTest", wc_test),
+    addparam("logFile", logfile),
+    
     addparam("Batchsize", batchsize),
     addparam("Rehash", rehash),
     addparam("Rebuild", rebuild),
@@ -31,11 +66,7 @@ slide = function(trainfile, testfile, batchsize=1000, rehash=1000, rebuild=1000,
     addparam("K", "2,6"),
     addparam("L", "20,50"),
     addparam("RangePow", "6,18"),
-    addparam("Sparsity", "1,0.005,1,1"),
-    
-    addparam("totRecords", 5000),
-    addparam("totRecordsTest", 500),
-    addparam("logFile", "/tmp/log.txt")
+    addparam("Sparsity", "1,0.005,1,1")
   )
   
   config_file = tempfile()
